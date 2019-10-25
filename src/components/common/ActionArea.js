8528db3a7c070793bb837882e315d55f497c76f1
Wendy Beck <wendy.e.beck@kp.org>
2019-10-25 16:58:35 -0700

    found props.children

--- src/components/common/ActionArea.js
+++ src/components/common/ActionArea.js
@@ -2,6 +2,7 @@ import React from 'react';
 import KpButton from '../formInputs/KpButton';
 
 const ActionArea = props => {
+	console.log('props: ', props);
 	return (
 		<div>
 			<KpButton>button</KpButton>
--- src/components/formInputs/KpButton.js
+++ src/components/formInputs/KpButton.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
+import { styled } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles => ({
+const MyButton = styled(Button)({
 	root: {
 		background: '#0078B3',
 		borderRadius: 0,
@@ -13,18 +13,21 @@ const useStyles = makeStyles => ({
 		textTransform: 'none',
 	},
 });
-
-const classes = useStyles();
+const ref = React.createRef('hello');
 
 const KpButton = props => {
-	const ref = React.createRef();
+	console.log('props: ', props);
+	console.log('ref: ', ref);
 	const handleClick = e => {
 		console.log('e: ', e.target);
 		ref.current.focus();
 	};
-
 	return (
-		<button ref={ref} onClick={handleClick} className={classes.root}></button>
+		<MyButton
+			ref={ref}
+			children={props.children}
+			onClick={handleClick}
+		></MyButton>
 	);
 };
 
